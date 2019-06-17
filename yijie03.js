186752c268c8f7eac297c6f1f17b6b2058942959
yijie <you@example.com>
2019-06-17 18:03:55 +0800

    0617 js 課程

---
+++ yijie03.js
@@ -0,0 +1,8 @@
+let var1;
+let var2 = 123;
+var1 = 1.3;
+document.write(typeof var1);
+const name = "yijie";
+document.write(`my name id ${name}`);
+let a = 10,b = 3;
+document.write(`${a}+${b}=${a+b}`)
